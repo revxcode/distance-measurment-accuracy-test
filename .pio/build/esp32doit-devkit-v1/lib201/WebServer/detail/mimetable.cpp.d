d48b0653e2a59256799e6d2422cb2ebda4d6a89d
revxcode <akunpo5521@gmail.com>
2025-10-21 10:30:45 +0700

    delete

--- .pio/build/esp32doit-devkit-v1/lib201/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib201/WebServer/detail/mimetable.cpp.o: \
- /home/ghoodev/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /home/ghoodev/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /home/ghoodev/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
--- .pio/build/esp32doit-devkit-v1/lib356/SD/sd_diskio_crc.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib356/SD/sd_diskio_crc.c.o: \
- /home/ghoodev/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
